3766f227d9ceffa813cdee4e1f057bbb87f15db3
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-10-28 17:00:28 +0100

    SQL : ajouter table commandes

---
+++ src/main/resources/db/changelog/V1create_commandes_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS commande (
+    id varchar(255) primary key ,
+    fournisseur_id varchar(255) not null ,
+    date_command DATETIME DEFAULT CURRENT_TIMESTAMP,
+    montant_total DECIMAL(10,2) DEFAULT 0.0,
+    statut ENUM('EN_ATTENTE','VALIDÉE','LIVRÉE','ANNULÉE') DEFAULT 'EN_ATTENTE',
+    FOREIGN KEY (fournisseur_id) references fournisseur(id)
+)
