9b289c99797610a5dc1dc426eeeb30783cc71075
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-10-29 14:10:00 +0100

    Feature : ajouter controller pour fournisseur

--- src/main/java/com/tricol/Tricol/controller/FournisseurController.java
+++ src/main/java/com/tricol/Tricol/controller/FournisseurController.java
@@ -1,4 +1,27 @@
 package com.tricol.Tricol.controller;
 
+import com.tricol.Tricol.dto.fournisseur.FournisseurResponseDto;
+import com.tricol.Tricol.service.serviceInterface.FournisseurService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/fournisseur")
 public class FournisseurController {
+    private final FournisseurService fournisseurService;
+
+    public FournisseurController(FournisseurService fournisseurService) {
+        this.fournisseurService = fournisseurService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FournisseurResponseDto>> findAll(){
+        List<FournisseurResponseDto> fournisseurs = fournisseurService.findAll();
+        return ResponseEntity.ok(fournisseurs);
+    }
+
 }
