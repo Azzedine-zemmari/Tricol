0fd0555b45bd80e7631b684adf98f5eb870c4094
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-10-29 12:00:00 +0100

    feature : ajouter interface service fourniseur avec method findAll et sa implementation

--- src/main/java/com/tricol/Tricol/service/FournisseurServiceImpl.java
+++ src/main/java/com/tricol/Tricol/service/FournisseurServiceImpl.java
@@ -1,23 +1,25 @@
 package com.tricol.Tricol.service;
 
-import com.tricol.Tricol.dto.fournisseur.FournisseurCreateDto;
+import com.tricol.Tricol.dto.fournisseur.FournisseurResponseDto;
 import com.tricol.Tricol.mapper.FournisseurMapper;
-import com.tricol.Tricol.model.Fournisseur;
 import com.tricol.Tricol.repository.FournisseurRepository;
+import com.tricol.Tricol.service.serviceInterface.FournisseurService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class FournisseurService {
+public class FournisseurServiceImpl implements FournisseurService {
     private final FournisseurRepository fournisseurRepository;
     private final FournisseurMapper fournisseurMapper;
-    public FournisseurService(FournisseurRepository repository , FournisseurMapper mapper) {
+
+    public FournisseurServiceImpl(FournisseurRepository repository , FournisseurMapper mapper) {
             this.fournisseurMapper = mapper;
             this.fournisseurRepository = repository;
     }
-    public List<FournisseurCreateDto> findAll() {
+    @Override
+    public List<FournisseurResponseDto> findAll() {
         return fournisseurRepository.findAll().stream()
                 .map(fournisseurMapper::toDTO)
                 .collect(Collectors.toList());
--- src/main/java/com/tricol/Tricol/service/serviceInterface/FournisseurService.java
+++ src/main/java/com/tricol/Tricol/service/serviceInterface/FournisseurService.java
@@ -1,4 +1,10 @@
 package com.tricol.Tricol.service.serviceInterface;
 
+import com.tricol.Tricol.dto.fournisseur.FournisseurResponseDto;
+
+import java.util.List;
+
+
 public interface FournisseurService {
+    List<FournisseurResponseDto> findAll();
 }
