d35491281cb1ed255ba047b88f0a4ba6b7cd4176
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-10-31 11:36:08 +0100

    Feature : ajouter produit repository

--- src/main/java/com/tricol/Tricol/repository/ProduitRepository.java
+++ src/main/java/com/tricol/Tricol/repository/ProduitRepository.java
@@ -3,5 +3,5 @@ package com.tricol.Tricol.repository;
 import com.tricol.Tricol.model.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Produit, String> {
+public interface ProduitRepository extends JpaRepository<Produit, String> {
 }
